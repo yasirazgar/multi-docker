7ec9079c60193ca22384dfc3e5a319185d282b37
yasirazgar <yasirazgar@gmail.com>
2021-08-18 22:30:17 +0530

    fix 1000

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
-docker build -t yasir6403/multi-client:latest -f ./client/Dockerfiler:$SHA ./client
-docker build -t yasir6403/multi-server:latest -f ./server/Dockerfiler:$SHA ./server
-docker build -t yasir6403/multi-worker:latest -f ./worker/Dockerfiler:$SHA ./worker
+docker build -t yasir6403/multi-client:latest -t yasir6403/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t yasir6403/multi-server:latest -t yasir6403/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t yasir6403/multi-worker:latest -t yasir6403/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push yasir6403/multi-client:latest
 docker push yasir6403/multi-server:latest
 docker push yasir6403/multi-worker:latest
+
 docker push yasir6403/multi-client:latest:$SHA
 docker push yasir6403/multi-server:latest:$SHA
 docker push yasir6403/multi-worker:latest:$SHA
+
 kubctl apply -f k8s
+
 kubectl set image deployments/client-deployment client=yasir6403/multi-client:$SHAR
 kubectl set image deployments/server-deployment server=yasir6403/multi-server:$SHAR
 kubectl set image deployments/worker-deployment worker=yasir6403/multi-worker:$SHAR
