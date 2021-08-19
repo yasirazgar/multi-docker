531887a5e622a31f866cc1294345bd28d0360b80
yasirazgar <yasirazgar@gmail.com>
2021-08-19 07:11:43 +0530

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ docker push yasir6403/multi-client:latest
 docker push yasir6403/multi-server:latest
 docker push yasir6403/multi-worker:latest
 
-docker push yasir6403/multi-client:latest:$SHA
-docker push yasir6403/multi-server:latest:$SHA
-docker push yasir6403/multi-worker:latest:$SHA
+docker push yasir6403/multi-client:$SHA
+docker push yasir6403/multi-server:$SHA
+docker push yasir6403/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=yasir6403/multi-client:$SHAR
-kubectl set image deployments/server-deployment server=yasir6403/multi-server:$SHAR
-kubectl set image deployments/worker-deployment worker=yasir6403/multi-worker:$SHAR
+kubectl set image deployments/client-deployment client=yasir6403/multi-client:$SHA
+kubectl set image deployments/server-deployment server=yasir6403/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=yasir6403/multi-worker:$SHA
